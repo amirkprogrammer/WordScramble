06e65a3221733f0876f1046a23706c5f7b166e36
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-01-20 13:30:20 -0800

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Amir Kabiri on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
